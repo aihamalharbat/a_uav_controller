c2c9615c998e873d14326ccb1088550861e14b42
Ayham Alharbat <a.n.alharbat@gmail.com>
2020-10-15 11:47:43 +0200

    ignore list updated
    cmake-build-debug folder added

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aalharbat/catkin_ws/src/a_uav_controller
-# Build directory: /home/aalharbat/catkin_ws/src/a_uav_controller/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
