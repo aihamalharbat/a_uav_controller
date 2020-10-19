1db089a567f35349f964136a31274f178ca89ff1
Ayham Alharbat <a.n.alharbat@gmail.com>
2020-10-19 16:11:35 +0200

    Adding math functions definitions.

--- include/a_uav_controller/controller.h
+++ include/a_uav_controller/controller.h
@@ -33,7 +33,7 @@ private:
 
 };
 
-// Functions for the controller
+// Math Functions for the controller
 inline void skewMatrixFromVector(const Eigen::Vector3d& vector,
                                  Eigen::Matrix3d* skew_matrix) {
     *skew_matrix << 0, -vector.z(), vector.y(), vector.z(), 0, -vector.x(),
@@ -43,6 +43,71 @@ inline void skewMatrixFromVector(const Eigen::Vector3d& vector,
 inline void vectorFromSkewMatrix(const Eigen::Matrix3d& skew_matrix,
                                  Eigen::Vector3d* vector) {
     *vector << skew_matrix(2, 1), skew_matrix(0, 2), skew_matrix(1, 0);
+}
+
+inline Eigen::Vector3d matrix_hat_inv(const Eigen::Matrix3d &m) {      // map from so(3) to R^3
+    Eigen::Vector3d v;
+    // TODO: Sanity checks if m is skew symmetric
+    v << m(7), m(2), m(3);
+    return v;
+}
+
+inline Eigen::Matrix3d matrix_hat(const Eigen::Vector3d &v) {
+    Eigen::Matrix3d m;
+    // Sanity checks on M
+    m << 0.0, -v(2), v(1), v(2), 0.0, -v(0), -v(1), v(0), 0.0;
+    return m;
+}
+
+inline Eigen::Matrix3d quat2RotMatrix(const Eigen::Vector4d &q) {
+    Eigen::Matrix3d rotmat;
+    rotmat << q(0) * q(0) + q(1) * q(1) - q(2) * q(2) - q(3) * q(3), 2 * q(1) * q(2) - 2 * q(0) * q(3),
+            2 * q(0) * q(2) + 2 * q(1) * q(3),
+
+            2 * q(0) * q(3) + 2 * q(1) * q(2), q(0) * q(0) - q(1) * q(1) + q(2) * q(2) - q(3) * q(3),
+            2 * q(2) * q(3) - 2 * q(0) * q(1),
+
+            2 * q(1) * q(3) - 2 * q(0) * q(2), 2 * q(0) * q(1) + 2 * q(2) * q(3),
+            q(0) * q(0) - q(1) * q(1) - q(2) * q(2) + q(3) * q(3);
+    return rotmat;
+}
+
+inline Eigen::Vector4d quatMultiplication(const Eigen::Vector4d &q, const Eigen::Vector4d &p) {
+    Eigen::Vector4d quat;
+    quat << p(0) * q(0) - p(1) * q(1) - p(2) * q(2) - p(3) * q(3), p(0) * q(1) + p(1) * q(0) - p(2) * q(3) + p(3) * q(2),
+            p(0) * q(2) + p(1) * q(3) + p(2) * q(0) - p(3) * q(1), p(0) * q(3) - p(1) * q(2) + p(2) * q(1) + p(3) * q(0);
+    return quat;
+}
+
+inline Eigen::Vector4d rot2Quaternion(const Eigen::Matrix3d &R) {
+    Eigen::Vector4d quat;
+    double tr = R.trace();
+    if (tr > 0.0) {
+        double S = sqrt(tr + 1.0) * 2.0;  // S=4*qw
+        quat(0) = 0.25 * S;
+        quat(1) = (R(2, 1) - R(1, 2)) / S;
+        quat(2) = (R(0, 2) - R(2, 0)) / S;
+        quat(3) = (R(1, 0) - R(0, 1)) / S;
+    } else if ((R(0, 0) > R(1, 1)) & (R(0, 0) > R(2, 2))) {
+        double S = sqrt(1.0 + R(0, 0) - R(1, 1) - R(2, 2)) * 2.0;  // S=4*qx
+        quat(0) = (R(2, 1) - R(1, 2)) / S;
+        quat(1) = 0.25 * S;
+        quat(2) = (R(0, 1) + R(1, 0)) / S;
+        quat(3) = (R(0, 2) + R(2, 0)) / S;
+    } else if (R(1, 1) > R(2, 2)) {
+        double S = sqrt(1.0 + R(1, 1) - R(0, 0) - R(2, 2)) * 2.0;  // S=4*qy
+        quat(0) = (R(0, 2) - R(2, 0)) / S;
+        quat(1) = (R(0, 1) + R(1, 0)) / S;
+        quat(2) = 0.25 * S;
+        quat(3) = (R(1, 2) + R(2, 1)) / S;
+    } else {
+        double S = sqrt(1.0 + R(2, 2) - R(0, 0) - R(1, 1)) * 2.0;  // S=4*qz
+        quat(0) = (R(1, 0) - R(0, 1)) / S;
+        quat(1) = (R(0, 2) + R(2, 0)) / S;
+        quat(2) = (R(1, 2) + R(2, 1)) / S;
+        quat(3) = 0.25 * S;
+    }
+    return quat;
 }
 
 #endif //A_UAV_CONTROLLER_CONTROLLER_H
